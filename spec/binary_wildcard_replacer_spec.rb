9e0dc042a992aeb770dbb794cc192fd006b33fcd
Daniel Lanciana <daniel@ivy.com>
2018-07-09 19:25:49 -0400

    refactor(test): move long output assertions to fixture

--- spec/binary_wildcard_replacer_spec.rb
+++ spec/binary_wildcard_replacer_spec.rb
@@ -4,6 +4,8 @@ require 'spec_helper'
 require 'binary_wildcard_replacer'
 
 describe '#binary_wildcard' do
+  let(:fixture) { IO.readlines('spec/fixtures/wildcard_test_output.txt') }
+
   it 'should print no wildcards' do
     expect(STDOUT).to receive(:puts).with('10110010100101')
     BinaryWildcardReplacer.replace '10110010100101'
@@ -30,38 +32,7 @@ describe '#binary_wildcard' do
   end
 
   it 'should print five wildcards' do
-    expect(STDOUT).to receive(:puts).with('0001000')
-    expect(STDOUT).to receive(:puts).with('0001001')
-    expect(STDOUT).to receive(:puts).with('0001010')
-    expect(STDOUT).to receive(:puts).with('0001011')
-    expect(STDOUT).to receive(:puts).with('0011000')
-    expect(STDOUT).to receive(:puts).with('0011001')
-    expect(STDOUT).to receive(:puts).with('0011010')
-    expect(STDOUT).to receive(:puts).with('0011011')
-    expect(STDOUT).to receive(:puts).with('0101000')
-    expect(STDOUT).to receive(:puts).with('0101001')
-    expect(STDOUT).to receive(:puts).with('0101010')
-    expect(STDOUT).to receive(:puts).with('0101011')
-    expect(STDOUT).to receive(:puts).with('0111000')
-    expect(STDOUT).to receive(:puts).with('0111001')
-    expect(STDOUT).to receive(:puts).with('0111010')
-    expect(STDOUT).to receive(:puts).with('0111011')
-    expect(STDOUT).to receive(:puts).with('1001000')
-    expect(STDOUT).to receive(:puts).with('1001001')
-    expect(STDOUT).to receive(:puts).with('1001010')
-    expect(STDOUT).to receive(:puts).with('1001011')
-    expect(STDOUT).to receive(:puts).with('1011000')
-    expect(STDOUT).to receive(:puts).with('1011001')
-    expect(STDOUT).to receive(:puts).with('1011010')
-    expect(STDOUT).to receive(:puts).with('1011011')
-    expect(STDOUT).to receive(:puts).with('1101000')
-    expect(STDOUT).to receive(:puts).with('1101001')
-    expect(STDOUT).to receive(:puts).with('1101010')
-    expect(STDOUT).to receive(:puts).with('1101011')
-    expect(STDOUT).to receive(:puts).with('1111000')
-    expect(STDOUT).to receive(:puts).with('1111001')
-    expect(STDOUT).to receive(:puts).with('1111010')
-    expect(STDOUT).to receive(:puts).with('1111011')
+    fixture.each { |line| expect(STDOUT).to receive(:puts).with(line.strip) }
     BinaryWildcardReplacer.replace 'XXX10XX'
   end
 end
